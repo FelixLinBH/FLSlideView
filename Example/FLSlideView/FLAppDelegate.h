f45fd788314f9d6bb3990039e1d3f8b3c178640c
felix.lin <fly_81211@hotmail.com>
2016-07-08 22:32:18 +0800

    Pod lib init.

---
+++ Example/FLSlideView/FLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FLAppDelegate.h
+//  FLSlideView
+//
+//  Created by felix.lin on 07/08/2016.
+//  Copyright (c) 2016 felix.lin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/FLSlideView/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FLSlideView
+//
+//  Created by felix.lin on 07/08/2016.
+//  Copyright (c) 2016 felix.lin. All rights reserved.
+//
+
+@import UIKit;
+#import "FLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FLAppDelegate class]));
+    }
+}
